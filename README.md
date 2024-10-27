eafb7fdd7a0f95cb05112cb4f3dfc84752f873b5
mohamed kamal <kamalmohammed837@gmail.com>
2024-10-28 01:17:58 +0300

    max side length

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# max side length
+# Max side length
+
+## problem
+Given a set of two-dimensional points, the goal is to compute the maximum side length of
+the square that can be drawn around each point, such that no two squares intersect,
+except at the sides.
+
+## input
+Single file that has several test cases.
+Each test case starts with the number of points on a single line, followed by each point on a separate line.
+All inputs will be integers.
+
+## output
+For each test case, output a single number on a separate line.
+
+## example
+### input
+```
+4
+1 1
+3 3
+5 5
+6 6
+3
+-4 2
+-3 7
+-3 -9
+```
+
+### output
+```
+1
+5
+```
--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,35 @@ import java.util.Comparator;
 
 public class Main {
 
-    public static long solve(String inputFile) {
+    public static void solve(String inputFile) {
         try {
             MaxSideLength mx = new MaxSideLength();
             BufferedReader br = new BufferedReader(new FileReader(inputFile));
-            int n = Integer.parseInt(br.readLine());
-            MaxSideLength.Point[] P = new MaxSideLength.Point[n];
-            for (int i = 0; i < n; i++) {
-                String[] line = br.readLine().split(" ");
-                int x = Integer.parseInt(line[0]);
-                int y = Integer.parseInt(line[1]);
-                P[i] = new MaxSideLength.Point(x, y);
+            String line;
+            while ((line = br.readLine()) != null) {
+                int n = Integer.parseInt(line);
+                MaxSideLength.Point[] P = new MaxSideLength.Point[n];
+                for (int i = 0; i < n; i++) {
+                    String[] pointLine = br.readLine().split(" ");
+                    int x = Integer.parseInt(pointLine[0]);
+                    int y = Integer.parseInt(pointLine[1]);
+                    P[i] = new MaxSideLength.Point(x, y);
+                }
+                MaxSideLength.Point[] X = P.clone(), Y = P.clone();
+                // sort the points by x and y coordinates
+                Arrays.sort(X, Comparator.comparingDouble(p -> p.x));
+                Arrays.sort(Y, Comparator.comparingDouble(p -> p.y));
+
+                double maxSideLength = mx.FindClosestPair(X, Y, 0, P.length - 1);
+                System.out.println((long) maxSideLength);
             }
-            MaxSideLength.Point[] X = P.clone(),Y=P.clone();
-            // sort the points by x and y coordinates
-            Arrays.sort(X, Comparator.comparingDouble(p -> p.x));
-            Arrays.sort(Y, Comparator.comparingDouble(p -> p.y));
-
             br.close();
-            double maxSideLength = mx.FindClosestPair(X,Y,0,P.length-1);
-
-            return (long) (maxSideLength);
         } catch (IOException e) {
-            return 0;
-        }
+            System.err.println("Error reading the input file: " + e.getMessage());        }
     }
 
     public static void main(String[] args) {
         String inputFile = System.getProperty("user.dir") + "\\src\\input.txt";
-        System.out.println( solve(inputFile));
-
+        solve(inputFile);
     }
 }
